3b4cfbe9f3e8766ab2ecedd8c279d223c49266ff
Youssef Mohammed <youssef.mu.saber@gmail.com>
2024-12-24 16:40:41 +0200

    Created the top app bar

---
+++ app/src/main/java/com/saber/todoapp/presentation/componants/TopBar.kt
@@ -0,0 +1,55 @@
+package com.saber.todoapp.presentation.componants
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import com.example.foodrecipe.presentation.componants.buttons.auth.LogoutButton
+import com.saber.todoapp.presentation.componants.buttons.BackArrowButton
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GeneralTopBar(
+    title: String,
+    isNavigationIcon: Boolean = false,
+    isLogoutIcon: Boolean = false,
+    onCLickCallBack: () -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            scrolledContainerColor = Transparent,
+            titleContentColor = Black,
+            containerColor = Transparent,
+            navigationIconContentColor = Black
+        ),
+        title = {
+            Text(
+                title,
+                maxLines = 1,
+                fontSize = 25.sp,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        navigationIcon = {
+            if (isNavigationIcon) BackArrowButton(
+                onClick = onCLickCallBack,
+                modifier = Modifier
+            )
+        },
+        actions = {
+            if (isLogoutIcon) {
+                LogoutButton(onLogoutClickable = {
+
+                })
+            }
+        }
+    )
+}
+
